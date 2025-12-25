661bc30dacfec99e858f23c48260e7f61496c481
MauroGutMB <mgmbarros2006@gmail.com>
2025-12-25 18:22:41 -0300

    Inicio do InsertionSort

---
+++ AlgoritmosDeOrdenacao/InsertionSort.c
@@ -0,0 +1,10 @@
+#include "InsertionSort.h"
+#include "../Vetor.h"
+#include <time.h>
+
+/*
+ * Insertion Sort
+ * \Complexidade Temporal: O(n^2) no pior caso
+ * \Complexidade Espacial: O(1)
+ * \Retorna clock_t com o tempo gasto na ordenação
+ * */
